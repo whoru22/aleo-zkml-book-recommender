2dbd0ab8fd9c38c69341cc682856345929ccc517
whoru22 ra <j1u2n3h4o7@gmail.com>
2024-03-13 03:14:08 +0000

    add zkml book recommender

---
+++ run.sh
@@ -0,0 +1,16 @@
+APPNAME="aleo_book_recommender_whoru"
+ENV_FILE=".env"
+PRIVATE_KEY=$(grep "PRIVATE_KEY" $ENV_FILE | cut -d '=' -f2)
+
+snarkos developer execute "${APPNAME}.aleo" main \
+32571i32 \
+32767i32 \
+32767i32 \
+0i32 \
+0i32 \
+0i32 \
+0i32 \
+  --private-key "${PRIVATE_KEY}" \
+  --query "https://api.explorer.aleo.org/v1" \
+  --priority-fee 3000000 \
+  --broadcast "https://api.explorer.aleo.org/v1/testnet3/transaction/broadcast" \
